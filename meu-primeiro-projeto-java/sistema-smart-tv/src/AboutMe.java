9d3d957629fd8f04909fef6ee363bafc24e976ff
Leandrocd77 <leandrocd@live.com>
2023-05-21 00:25:48 -0300

    AboutMe

--- meu-primeiro-projeto-java/sistema-smart-tv/src/AboutMe.java
+++ meu-primeiro-projeto-java/sistema-smart-tv/src/AboutMe.java
@@ -3,25 +3,24 @@ import java.util.Scanner;
 
 public class AboutMe {
     public static void  main(String[] args) {
-      Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-      
-      
-      System.out.println("Digite seu nome");
-      String nome = scanner.next();
+      try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+        System.out.println("Digite seu nome");
+          String nome = scanner.next();
 
-      System.out.println("Digite seu sobrenome");
-      String sobrenome = scanner.next();
+          System.out.println("Digite seu sobrenome");
+          String sobrenome = scanner.next();
 
-      System.out.println("Digite sua idade");
-      int idade = scanner.nextInt();
+          System.out.println("Digite sua idade");
+          int idade = scanner.nextInt();
 
-      System.out.println("Digite sua altura");
-      double altura = scanner.nextDouble();
+          System.out.println("Digite sua altura");
+          double altura = scanner.nextDouble();
 
-    // imprimindo os dados obtidos pelo usuário 
-      System.out.println("Olá, me chamo " +  nome + " " + sobrenome);
-      System.out.println("Tenho " + idade + "anos ");
-      System.out.println("Minha altura é " + altura + " cm ");
+        // imprimindo os dados obtidos pelo usuário 
+          System.out.println("Olá, me chamo " +  nome + " " + sobrenome);
+          System.out.println("Tenho " + idade + "anos ");
+          System.out.println("Minha altura é " + altura + " cm ");
+    }
     }
 }
 
